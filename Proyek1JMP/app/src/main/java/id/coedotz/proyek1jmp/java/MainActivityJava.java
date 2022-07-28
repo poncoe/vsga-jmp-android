493bc7563b072481c6f0616e7204839dd3114207
poncoe <poncoe4mac@gmail.com>
2022-07-28 10:26:06 +0700

    added menu in logic main

--- Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/java/MainActivityJava.java
+++ Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/java/MainActivityJava.java
@@ -1,7 +1,12 @@
 package id.coedotz.proyek1jmp.java;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import id.coedotz.proyek1jmp.R;
 
@@ -11,5 +16,24 @@ public class MainActivityJava extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_java);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenujava, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu1) {
+          //  startActivity(new Intent(this, Menu1Java.class));
+        } else if (item.getItemId() == R.id.menu2) {
+           // startActivity(new Intent(this, Menu2Java.class));
+        } else if (item.getItemId() == R.id.menu3) {
+           // startActivity(new Intent(this, Menu3Java.class));
+        }
+        return true;
     }
 }
--- Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/MainActivity.kt
+++ Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/MainActivity.kt
@@ -1,12 +1,32 @@
 package id.coedotz.proyek1jmp.kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import id.coedotz.proyek1jmp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.mainmenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu1) {
+           // startActivity(Intent(this, Menu1Java::class.java))
+        } else if (item.itemId == R.id.menu2) {
+           // startActivity(Intent(this, Menu2Java::class.java))
+        } else if (item.itemId == R.id.menu3) {
+           // startActivity(Intent(this, Menu3Java::class.java))
+        }
+        return true
     }
 }
