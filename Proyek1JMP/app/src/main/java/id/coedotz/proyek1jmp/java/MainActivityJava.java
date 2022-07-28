761b086296c1cb8db6b180341bdad18cda9b2be5
poncoe <poncoe4mac@gmail.com>
2022-07-28 10:11:43 +0700

    reflactoring kotlin to java in package java

---
+++ Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/java/MainActivityJava.java
@@ -0,0 +1,15 @@
+package id.coedotz.proyek1jmp.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.coedotz.proyek1jmp.R;
+
+public class MainActivityJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_java);
+    }
+}
