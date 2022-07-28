87c5d425351c4335e3a1e6b973471faa452cc014
poncoe <poncoe4mac@gmail.com>
2022-07-28 10:07:40 +0700

    create activity java & reflactoring activity kotlin

--- Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/MainActivity.kt
+++ Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.coedotz.proyek1jmp
+package id.coedotz.proyek1jmp.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.coedotz.proyek1jmp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
