20a9b01a8080c77bfea963e4f50a0f7e8cca8ebd
poncoe <poncoe4mac@gmail.com>
2022-08-01 14:56:24 +0700

    logic kotlin kalkulator & input nama

--- Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/InputDataKotlin.kt
+++ Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/InputDataKotlin.kt
@@ -1,12 +1,24 @@
 package id.coedotz.proyek1jmp.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import id.coedotz.proyek1jmp.R
 
 class InputDataKotlin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_data_kotlin)
+
+        val etNama = findViewById<EditText>(R.id.input_nama)
+        val btnProses = findViewById<Button>(R.id.btn_proses)
+        val txtHasil = findViewById<TextView>(R.id.txt_hasil)
+
+        btnProses.setOnClickListener {
+            val nama = etNama.text.toString()
+            txtHasil.text = nama
+        }
     }
 }
--- Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/KalkulatorKotlin.kt
+++ Proyek1JMP/app/src/main/java/id/coedotz/proyek1jmp/kotlin/KalkulatorKotlin.kt
@@ -1,12 +1,65 @@
 package id.coedotz.proyek1jmp.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import id.coedotz.proyek1jmp.R
 
 class KalkulatorKotlin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalkulator_kotlin)
+
+        val etAngka1 = findViewById<EditText>(R.id.input_angka1)
+        val etAngka2 = findViewById<EditText>(R.id.input_angka2)
+
+        val btnTambah = findViewById<Button>(R.id.btn_plus)
+        val btnKurang = findViewById<Button>(R.id.btn_kurang)
+        val btnKali = findViewById<Button>(R.id.btn_kali)
+        val btnBagi = findViewById<Button>(R.id.btn_bagi)
+        val btnBersihkan = findViewById<Button>(R.id.btn_bersihkan)
+
+        val txtHasil = findViewById<TextView>(R.id.txt_hasil_kalkulator)
+
+        // TAMBAH
+        btnTambah.setOnClickListener {
+            val angka1 = etAngka1.text.toString().toDouble()
+            val angka2 = etAngka2.text.toString().toDouble()
+            val hasil = angka1 + angka2
+            txtHasil.text = "Hasil\n$hasil"
+        }
+
+        // KURANG
+        btnKurang.setOnClickListener {
+            val angka1 = etAngka1.text.toString().toDouble()
+            val angka2 = etAngka2.text.toString().toDouble()
+            val hasil = angka1 - angka2
+            txtHasil.text = "Hasil\n$hasil"
+        }
+
+        // KALI
+        btnKali.setOnClickListener {
+            val angka1 = etAngka1.text.toString().toDouble()
+            val angka2 = etAngka2.text.toString().toDouble()
+            val hasil = angka1 * angka2
+            txtHasil.text = "Hasil\n$hasil"
+        }
+
+        // BAGI
+        btnBagi.setOnClickListener {
+            val angka1 = etAngka1.text.toString().toDouble()
+            val angka2 = etAngka2.text.toString().toDouble()
+            val hasil = angka1 / angka2
+            txtHasil.text = "Hasil\n$hasil"
+        }
+
+        // BERSIHKAN
+        btnBersihkan.setOnClickListener {
+            etAngka1.setText("")
+            etAngka2.setText("")
+            txtHasil.text = ""
+        }
     }
 }
