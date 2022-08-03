df6a66de21bd68ea9f78a409e8b39b3d024f4f81
poncoe <poncoe4mac@gmail.com>
2022-08-03 18:12:53 +0700

    start proyek2 jmp

---
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/ListNegaraKotlin.kt
@@ -0,0 +1,12 @@
+package id.coedotz.proyek2jmp.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.coedotz.proyek2jmp.R
+
+class ListNegaraKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_negara_kotlin)
+    }
+}
--- Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/MainActivity.kt
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.coedotz.proyek2jmp
+package id.coedotz.proyek2jmp.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.coedotz.proyek2jmp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
