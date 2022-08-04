f17ed9f718dedd9c3a65473386642d5376f34b5b
poncoe <poncoe4mac@gmail.com>
2022-08-04 18:37:02 +0700

    reflactor java

---
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/java/ListNegaraJava.java
@@ -0,0 +1,16 @@
+package id.coedotz.proyek2jmp.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.coedotz.proyek2jmp.R;
+
+public class ListNegaraJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_negara_java);
+    }
+}
---
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/java/MainActivityJava.java
@@ -0,0 +1,16 @@
+package id.coedotz.proyek2jmp.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.coedotz.proyek2jmp.R;
+
+public class MainActivityJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_java);
+    }
+}
