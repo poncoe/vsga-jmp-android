558ddeb59b6ec977f8743a8a093ddd07f7eb9af1
poncoe <poncoe4mac@gmail.com>
2022-08-04 18:46:42 +0700

    add logic login in mainclass

--- Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/java/MainActivityJava.java
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/java/MainActivityJava.java
@@ -2,7 +2,12 @@ package id.coedotz.proyek2jmp.java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import id.coedotz.proyek2jmp.R;
 
@@ -12,5 +17,26 @@ public class MainActivityJava extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_java);
+
+        EditText etUsername = findViewById(R.id.input_username);
+        EditText etPassword = findViewById(R.id.input_password);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if(username.equalsIgnoreCase("jmp-a") && password.equalsIgnoreCase("123")){
+                    Intent intent = new Intent(MainActivityJava.this, ListNegaraJava.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivityJava.this, "Password atau Username salah!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/MainActivity.kt
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/MainActivity.kt
@@ -1,12 +1,37 @@
 package id.coedotz.proyek2jmp.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import id.coedotz.proyek2jmp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etUsername = findViewById<EditText>(R.id.input_username)
+        val etPassword = findViewById<EditText>(R.id.input_password)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+            if (username.equals("jmp-a", ignoreCase = true) && password.equals("123", ignoreCase = true)) {
+                val intent = Intent(this@MainActivity, ListNegaraKotlin::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Password atau Username salah!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
