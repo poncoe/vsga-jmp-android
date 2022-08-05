7c607664155194e80580d4c7feb31d37714b1e78
poncoe <poncoe4mac@gmail.com>
2022-08-05 19:11:05 +0700

    listnegara proyek2

--- Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/java/ListNegaraJava.java
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/java/ListNegaraJava.java
@@ -3,6 +3,14 @@ package id.coedotz.proyek2jmp.java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import id.coedotz.proyek2jmp.R;
 
@@ -12,5 +20,34 @@ public class ListNegaraJava extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_negara_java);
+
+        ArrayList<String> listNegara = new ArrayList<>();
+        listNegara.add("Indonesia");
+        listNegara.add("Malaysia");
+        listNegara.add("Thailand");
+        listNegara.add("Kamboja");
+        listNegara.add("Philipina");
+
+        EditText etInputNegara = findViewById(R.id.input_negara);
+        Button btnSubmit = findViewById(R.id.btn_submit);
+        ListView listViewNegara = findViewById(R.id.lv_negara);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listNegara);
+        listViewNegara.setAdapter(adapter);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String negara = etInputNegara.getText().toString();
+
+                if(!negara.equalsIgnoreCase("")){
+                    //adapter.add(negara);
+                    listNegara.add(negara);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(ListNegaraJava.this, "Isian Kosong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/ListNegaraKotlin.kt
+++ Proyek2JMP/app/src/main/java/id/coedotz/proyek2jmp/kotlin/ListNegaraKotlin.kt
@@ -1,12 +1,38 @@
 package id.coedotz.proyek2jmp.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import id.coedotz.proyek2jmp.R
 
 class ListNegaraKotlin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_negara_kotlin)
+
+        val listNegara = ArrayList<String>()
+        listNegara.add("Indonesia")
+        listNegara.add("Malaysia")
+        listNegara.add("Thailand")
+        listNegara.add("Kamboja")
+        listNegara.add("Philipina")
+
+        val etInputNegara = findViewById<EditText>(R.id.input_negara)
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
+        val listViewNegara = findViewById<ListView>(R.id.lv_negara)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listNegara)
+        listViewNegara.adapter = adapter
+
+        btnSubmit.setOnClickListener {
+            val negara = etInputNegara.text.toString()
+            if (!negara.equals("", ignoreCase = true)) {
+                //adapter.add(negara);
+                listNegara.add(negara)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(this@ListNegaraKotlin, "Isian Kosong!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
